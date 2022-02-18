61172c4987e77180f5eec096f48d3610c3fda26d
EliWimmer <48037550+EliWimmer@users.noreply.github.com>
2022-02-18 15:27:34 -0700

    bits and bobs

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,61 +1,69 @@
-import browser from "webextension-polyfill";
+import browser from 'webextension-polyfill'
 
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.local.set({
-        global: {
-            theme: "--nst_theme-notion-default", 
-            sliders: {
-                "--nst_tweak-last-props-first": [0]
-            }
-        },
-    });
-    chrome.storage.local.set({
-        local: {
-            },
-    });
-    chrome.storage.local.set({
-        settings: {
-            darkMode: true,
-        },
-    });
-    chrome.storage.local.set({
-        meta: {
-            activePage: "",
-            scopeMode: "global",
-        },
-    });
-});
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.local.set({
+    global: {
+      theme: '--nst_theme-notion-default',
+      sliders: {
+        '--nst_tweak-last-props-first': [0],
+      },
+    },
+  })
+  chrome.storage.local.set({
+    local: {},
+  })
+  chrome.storage.local.set({
+    settings: {
+      darkMode: true,
+    },
+  })
+  chrome.storage.local.set({
+    meta: {
+      activePage: '',
+      scopeMode: 'global',
+    },
+  })
+})
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    let uuid;
-    if (tab.url.lastIndexOf("-") != -1) {
-        uuid = tab.url.substring(tab.url.lastIndexOf("-") + 1);
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    console.log(tab.url.substring(0, 22), changeInfo.status);
+  if (
+    // if tab.url starts with 'https://notion.so/' and changeInfo.status === 'complete'
+    tab.url.substring(0, 22) == 'https://www.notion.so/' 
+  ) {
+    console.log("This ran");
+    let uuid
+    if (tab.url.lastIndexOf('-') != -1) {
+      uuid = tab.url.substring(tab.url.lastIndexOf('-') + 1)
     } else {
-        uuid = tab.url.substring(tab.url.lastIndexOf("/") + 1).substring(0, 32);
+      uuid = tab.url.substring(tab.url.lastIndexOf('/') + 1).substring(0, 32)
     }
     let activePage = {
-        title: tab.title,
-        uuid: uuid,
-        icon: tab.favIconUrl,
-        tabid: tab.id,
-    };
+      title: tab.title,
+      uuid: uuid,
+      icon: tab.favIconUrl,
+      tabid: tab.id,
+    }
     let meta = {}
-    chrome.storage.local.get("meta", (data) => {
-        Object.assign(meta, data.meta)
-        meta.activePage = activePage;
-        chrome.storage.local.set({ meta });
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            files: ["build/update.js"],
-        });
-    });
-    let local = {};
-    chrome.storage.local.get("local", (data) => {
-        Object.assign(local, data.local)
-        if (local[uuid] == undefined) {
-            local[uuid] = {sliders: {"--nst_tweak-last-props-first": [0]},
-        theme: "--nst_theme-notion-default",};
+    chrome.storage.local.get('meta', (data) => {
+      Object.assign(meta, data.meta)
+      meta.activePage = activePage
+      chrome.storage.local.set({ meta })
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['build/update.js'],
+      })
+    })
+    let local = {}
+    chrome.storage.local.get('local', (data) => {
+      Object.assign(local, data.local)
+      if (local[uuid] == undefined) {
+        local[uuid] = {
+          sliders: { '--nst_tweak-last-props-first': [0] },
+          theme: '--nst_theme-notion-default',
         }
-        chrome.storage.local.set({ local });
-    });
-});
+      }
+      chrome.storage.local.set({ local })
+    })
+  }
+})
--- src/scripts/dist/background.dev.js
+++ src/scripts/dist/background.dev.js
@@ -7,9 +7,9 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.local.set({
     global: {
-      theme: "--nst_theme-notion-default",
+      theme: '--nst_theme-notion-default',
       sliders: {
-        "--nst_tweak-last-props-first": [0]
+        '--nst_tweak-last-props-first': [0]
       }
     }
   });
@@ -23,55 +23,61 @@ chrome.runtime.onInstalled.addListener(function () {
   });
   chrome.storage.local.set({
     meta: {
-      activePage: "",
-      scopeMode: "global"
+      activePage: '',
+      scopeMode: 'global'
     }
   });
 });
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  var uuid;
+  console.log(tab.url.substring(0, 22), changeInfo.status);
 
-  if (tab.url.lastIndexOf("-") != -1) {
-    uuid = tab.url.substring(tab.url.lastIndexOf("-") + 1);
-  } else {
-    uuid = tab.url.substring(tab.url.lastIndexOf("/") + 1).substring(0, 32);
-  }
+  if ( // if tab.url starts with 'https://notion.so/' and changeInfo.status === 'complete'
+  tab.url.substring(0, 22) == 'https://www.notion.so/') {
+    console.log("This ran");
+    var uuid;
 
-  var activePage = {
-    title: tab.title,
-    uuid: uuid,
-    icon: tab.favIconUrl,
-    tabid: tab.id
-  };
-  var meta = {};
-  chrome.storage.local.get("meta", function (data) {
-    Object.assign(meta, data.meta);
-    meta.activePage = activePage;
-    chrome.storage.local.set({
-      meta: meta
-    });
-    chrome.scripting.executeScript({
-      target: {
-        tabId: tab.id
-      },
-      files: ["build/update.js"]
-    });
-  });
-  var local = {};
-  chrome.storage.local.get("local", function (data) {
-    Object.assign(local, data.local);
-
-    if (local[uuid] == undefined) {
-      local[uuid] = {
-        sliders: {
-          "--nst_tweak-last-props-first": [0]
-        },
-        theme: "--nst_theme-notion-default"
-      };
+    if (tab.url.lastIndexOf('-') != -1) {
+      uuid = tab.url.substring(tab.url.lastIndexOf('-') + 1);
+    } else {
+      uuid = tab.url.substring(tab.url.lastIndexOf('/') + 1).substring(0, 32);
     }
 
-    chrome.storage.local.set({
-      local: local
+    var activePage = {
+      title: tab.title,
+      uuid: uuid,
+      icon: tab.favIconUrl,
+      tabid: tab.id
+    };
+    var meta = {};
+    chrome.storage.local.get('meta', function (data) {
+      Object.assign(meta, data.meta);
+      meta.activePage = activePage;
+      chrome.storage.local.set({
+        meta: meta
+      });
+      chrome.scripting.executeScript({
+        target: {
+          tabId: tab.id
+        },
+        files: ['build/update.js']
+      });
     });
-  });
+    var local = {};
+    chrome.storage.local.get('local', function (data) {
+      Object.assign(local, data.local);
+
+      if (local[uuid] == undefined) {
+        local[uuid] = {
+          sliders: {
+            '--nst_tweak-last-props-first': [0]
+          },
+          theme: '--nst_theme-notion-default'
+        };
+      }
+
+      chrome.storage.local.set({
+        local: local
+      });
+    });
+  }
 });
